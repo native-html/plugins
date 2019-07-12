d47a4a44082f11cc9ca328d2658b3167bfb0b3c5
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2019-07-12 16:48:33 +0200

    run injected javascript in a closure
    attempt fix #4

--- src/HTMLTable/script.ts
+++ src/HTMLTable/script.ts
@@ -1,45 +1,45 @@
-
 const injectedScript = `
+(function () {
+  var RNWV = window.ReactNativeWebView;
 
-var RNWV = window.ReactNativeWebView;
-
-// Send size on body content height updates
-function postSize() {
-  var tables = document.getElementsByTagName('table');
-  if (tables.length > 0) {
-    var table = tables[0];
-    var tableHeight = table.scrollHeight;
-    RNWV.postMessage(JSON.stringify({
-      type: 'heightUpdate',
-      content: tableHeight
-    }));
-  }
-}
-postSize();
-//trigger when DOM changes
-var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-var observer = new MutationObserver(postSize);
-observer.observe(document, {
-    subtree: true,
-    attributes: true
-});
-
-// Intercept click events
-
-function interceptClickEvent(e) {
-  var href;
-  var target = e.target || e.srcElement;
-  if (target.tagName === 'A') {
-      href = target.getAttribute('href');
-      e.preventDefault();
-      // Post message
+  // Send size on body content height updates
+  function postSize() {
+    var tables = document.getElementsByTagName('table');
+    if (tables.length > 0) {
+      var table = tables[0];
+      var tableHeight = table.scrollHeight;
       RNWV.postMessage(JSON.stringify({
-        type: 'navigateEvent',
-        content: href
+        type: 'heightUpdate',
+        content: tableHeight
       }));
+    }
   }
-}
+  postSize();
+  //trigger when DOM changes
+  var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+  var observer = new MutationObserver(postSize);
+  observer.observe(document, {
+      subtree: true,
+      attributes: true
+  });
 
-document.addEventListener('click', interceptClickEvent);
+  // Intercept click events
+
+  function interceptClickEvent(e) {
+    var href;
+    var target = e.target || e.srcElement;
+    if (target.tagName === 'A') {
+        href = target.getAttribute('href');
+        e.preventDefault();
+        // Post message
+        RNWV.postMessage(JSON.stringify({
+          type: 'navigateEvent',
+          content: href
+        }));
+    }
+  }
+
+  document.addEventListener('click', interceptClickEvent);
+})();
 `
 export default injectedScript
